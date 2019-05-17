a700f602088e30a2246a3beaccebde9c8a137758
Regan Barton <9996805@student.toiohomai.ac.nz>
2019-05-17 19:44:48 +1200

    Created project

---
+++ Match_3/Match_3_Task/Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public GameObject TilePrefab;
+    private Background[,] Tiles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tiles = new Background[Width, Height];
+        SetUp();
+
+    }
+
+    private void SetUp()
+    {
+        for ( int i = 0; i < Width; i++)
+        {
+           for (int w = 0; w < Height; w++)
+            {
+                Vector2 tempPosition = new Vector2(i, w);
+                Instantiate(TilePrefab, tempPosition ,Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
